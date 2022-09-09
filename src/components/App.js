ed2ff57e932461fc33b411e69058a93f5182684c
Brian H Suarez <38057048+bhsuarez@users.noreply.github.com>
2022-09-09 18:48:51 -0500

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { GlobalStyle, ThemeProvider } from '@react95/core';
+import { GlobalStyle, ThemeProvider, Button } from '@react95/core';
 import { createGlobalStyle } from 'styled-components';
 import DataService from '../services/dataService';
 import DataContext from '../contexts/dataContext';
 import Taskbar from './Taskbar';
 import Desktop from './Desktop';
+import { useClippy, ClippyProvider } from '@react95/clippy';
 
 const dataService = new DataService();
 
@@ -13,6 +14,12 @@ const BodyFontSizeOverride = createGlobalStyle`
     font-size: 15px
   }
 `;
+
+const ClippyComponent = () => {
+  const { clippy } = useClippy();
+
+  return <Button onClick={() => clippy.play('Wave')}>Hello Clippy!</Button>;
+};
 
 const App = () => (
   <DataContext.Provider value={dataService}>
@@ -23,6 +30,10 @@ const App = () => (
       <Desktop />
       <Taskbar />
     </ThemeProvider>
+    <ClippyProvider>
+      <ClippyComponent />
+    </ClippyProvider>
   </DataContext.Provider>
 );
+
 export default App;
